0b79cb1f7af3e954e3040421ccdfbcb2eead4315
dmoore5050 <dustinmoore@gmail.com>
2013-04-16 10:19:05 -0500

    minor dumb additions

--- yeah.rb
+++ yeah.rb
@@ -1,10 +1,14 @@
 puts "Hi! What's your name?"
 name = gets.chomp
-name.each_char do |letter| 
+name.downcase.each_char do |letter| 
 	letter_check = ["a","e","f","h","i","l","m","n","o","r","s","x"]
 	if letter_check.include?( letter ) 
 		article = "an"
 	else
 		article = "a"
 	end	
-	puts "\# Give me #{article} #{letter.upcase}!" end
+	puts "\# Give me #{article} #{letter.upcase}!" 
+end
+random_string = (0...6).map{(65+rand(26)).chr}.join
+puts "What's that spell? #{random_string.capitalize}!"
+puts "Err... what? Sorry! Goooo #{name.capitalize}!!!"
